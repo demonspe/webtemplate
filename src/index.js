dc435d2685425e6a1b6b2de5c7c39e2a80b19ffe
Решетников Александр (Reshetnikov_AS) <Reshetnikov_AS@directum.ru>
2016-10-26 15:16:53 +0400

    Пустой шаблон проекта.

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//@flow
+
+import React from 'react';
+import { render } from 'react-dom';
+
+require('./styles/app.css');
+
+render(
+  <div>Hello world</div>,
+  document.getElementById('root')
+);
